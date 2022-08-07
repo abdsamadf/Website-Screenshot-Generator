2cfdbe0ff55eb03d510408dad70dee628d4f9f35
abdsamadf <abdulsamadf97@gmail.com>
2022-08-07 21:30:35 +0500

    update signin

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -69,6 +69,8 @@ const SignIn = () => {
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   type="password"
                   id="password"
+                  autoComplete="current-password"
+                  required
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
