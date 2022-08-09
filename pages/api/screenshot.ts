ff2ad222420c8f153aeefb4d51d48babcb3fe11b
abdsamadf <abdulsamadf97@gmail.com>
2022-08-09 18:57:44 +0500

    updated

--- pages/api/screenshot.ts
+++ pages/api/screenshot.ts
@@ -1,9 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { supabase } from "../../src/utils/SupabaseClient";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 // import { uuid } from 'uuidv4';
-const puppeteer = require("puppeteer");
+// const puppeteer = require("puppeteer");
+// /* Importing the puppeteer library. */
+const puppeteer = require('puppeteer-core')
+// import puppeteer from "puppeteer-core";
+// import * as puppeteer from 'puppeteer';
+// import chromium from "chrome-aws-lambda";
+// import playwright from "playwright-core";
 // import puppeteer from 'puppeteer';
 
 type Data = {
@@ -14,7 +20,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-
   const userId = req.body.uid;
   const url = req.body.url;
   const uniqueImageId = uuidv4();
@@ -31,6 +36,23 @@ export default async function handler(
     });
 
   async function takeScreenshot() {
+    /*     const browser = await chromium.puppeteer.launch({
+      args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    }); */
+/*
+    const browser = await playwright.chromium.launch({
+      args: chromium.args,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    });
+ */
+  //   const browser = await chromium.puppeteer.launch({
+  //     // executablePath: await chromium.executablePath,
+  // });
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: "networkidle2" });
@@ -40,11 +62,10 @@ export default async function handler(
     // save buffer screenshot to supabase storage
     const { data, error } = await supabase.storage
       .from("screenshot-bucket")
-      .upload(`public/${userId}/${uniqueImageId}.jpg`, buffer)
+      .upload(`public/${userId}/${uniqueImageId}.jpg`, buffer);
 
     await page.close();
     await browser.close();
-
   }
 
   // delay for 1 second for save screenshot to supabase storage
@@ -52,5 +73,4 @@ export default async function handler(
 
   res.redirect("/");
   // res.status(200).json({ name: "Successful" });
-
 }
